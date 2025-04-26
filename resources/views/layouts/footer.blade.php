1607f00f3dce26bd3b1a6a5a73a1eb1fe4488e38
saliansyah <dhafinsaliansyah@gmail.com>
2025-04-26 09:56:59 +0700

    layout footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="footer">
+    <div class="container">
+      <div class="row d-flex justify-content-between">
+        <div class="col">
+          <h3 class="fw-bold">CreativeMusic<span class="hub">Hub</span></h3>
+          <p class="desc">
+            <i class="fa-solid fa-location-dot" style="margin-right: 8px;"></i>
+        Komplek Bandung Indah Raya, Blok C13/No.17, Kelurahan Mekarjaya, Kecamatan Rancasari, Kota Bandung, Jawa Barat 40286
+
+
+          </p>
+          <div class="no mb-1 mt-2">
+            <a href="#">
+              <i class="fa-brands fa-whatsapp" style="margin-right: 8px;"></i>
+              <p class="m-0">+62 822-9560-3115</p>
+            </a>
+          </div>
+          <div class="mail">
+            <a href="mailto:info@creativemusichub.com">
+              <i class="fa-regular fa-envelope" style="margin-right: 8px;"></i>
+              <p class="m-0">info@creativemusichub.com</p>
+            </a>
+          </div>
+          <div class="social mt-3">
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-solid fa-globe"></i>
+            <i class="fa-regular fa-envelope"></i>
+          </div>
+        </div>
+
+        <div class="col d-flex flex-column col-lg-4 col mt-lg-0 mt-5">
+          <h5 class="fw-bold">Our Products</h5>
+          <a href="/product1">Digital Beat Maker</a>
+          <a href="/product2">Guitar Tuner Pro</a>
+          <a href="/product3">Online Studio</a>
+          <a href="/product4">Custom Sound FX</a>
+        </div>
+
+        <div class="col d-flex flex-column col-lg-3 col mt-lg-0 mt-5">
+          <h5 class="fw-light">
+            Total Visitors: <span class="pengunjung">1,234</span>
+          </h5>
+          <div class="elfsight-app-954f2dc6-9353-4086-83d2-32455852907f"
+               data-elfsight-app-lazy
+               data-elfsight-counter-value="1234"></div>
+        </div>
+      </div>
+
+
+    </div>
+  </footer>
